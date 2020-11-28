29dc301a15f9328ff42e788e20140d3b49b82924
Cainie <karol.marszalik@gmail.com>
2020-11-28 22:10:24 +0100

    Added animations to player & tweaked scene

--- Assets/Scripts/TopDownMovement.cs
+++ Assets/Scripts/TopDownMovement.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class TopDownMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D _rigidbody2D;
+    private Vector2 _movement;
+    private Animator _animator;
+     
+
+    private void Awake()
     {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+    
+    private void Update()
+    {
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y =Input.GetAxisRaw("Vertical");
         
+        _animator.SetFloat("Horizontal", _movement.x);
+        _animator.SetFloat("Vertical", _movement.y);
+        _animator.SetFloat("Speed", _movement.sqrMagnitude);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        _rigidbody2D.MovePosition(_rigidbody2D.position + _movement * (moveSpeed * Time.fixedDeltaTime));
     }
 }
